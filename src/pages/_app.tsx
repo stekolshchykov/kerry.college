bc77afb4e327cc8d506b5338f880ff0c981238ee
mk <v.a.stekolschikov@gmail.com>
2024-10-15 20:31:50 +0100

    added Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import {RootStoreProvider} from "@/providers/RootStoreProvider";
 import FooterUi from "@/ui/footer-ui";
 import HeaderUi from "@/ui/header-ui";
+import {Analytics} from "@vercel/analytics/react"
 
 import type {AppProps} from 'next/app';
 import Head from "next/head";
@@ -19,6 +20,7 @@ function MyApp({Component}: AppProps) {
             <HeaderUi/>
             <Component/>
             <FooterUi/>
+            <Analytics/>
         </RootStoreProvider>
     );
 }
