ae830a4f4ff2293b257d85375fae63ad85c66744
mk <v.a.stekolschikov@gmail.com>
2024-10-16 10:18:28 +0100

    added staff data

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -5,11 +5,11 @@ import React from "react";
 
 const StaffCard = (props: { name: string, img: string, job: string }) => {
 
-    const imgPath = (props?.img && props?.img?.length > 3) ? props?.img : "/staff/no-img.webp"
+    const imgPath = (props?.img && props?.img?.length > 3) ? props?.img : "/staff/no-img.jpg"
 
     return <li className={"flex flex-column gap-0 leading-[1.2]"}>
         <div className={`
-            w-full min-h-[350px] max-h-[350px] overflow-hidden
+            w-full min-h-[250px] max-h-[250px] overflow-hidden
             mb-1
         `}
              style={{
@@ -25,8 +25,7 @@ const StaffCard = (props: { name: string, img: string, job: string }) => {
 }
 
 const Staff = () => {
-
-
+    
     return <>
 
         <PageLayout isContainer={true} className={"bg-mina text-white"}>
@@ -34,7 +33,7 @@ const Staff = () => {
         </PageLayout>
 
         <PageLayout isContainer={true} className={"mb-[25px] mt-[50px]"}>
-            <ul className={"grid grid-cols-2 lg:grid-cols-4 gap-4 justify-between"}>
+            <ul className={"grid grid-cols-2 2xl:grid-cols-10 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2  gap-4 justify-between"}>
                 {dataStaff.map((s, k) => <StaffCard key={k} job={s.job} name={s.name} img={s.img}/>)}
             </ul>
         </PageLayout>
