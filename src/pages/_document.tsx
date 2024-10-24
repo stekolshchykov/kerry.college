dbd41c42bc56c5137bab894891df4f4030d6e187
mk <v.a.stekolschikov@gmail.com>
2024-10-24 12:02:44 +0100

    updated ui, email, favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-
+                    <link rel="icon" href="/favicon.png"/>
                 </Head>
                 <body>
                 <Main/>
--- src/ui/content-maestro-ui.tsx
+++ src/ui/content-maestro-ui.tsx
@@ -8,7 +8,7 @@ const ContentMaestroUi = () => {
             <div className={"flex gap-[25px] py-[50px] flex-col md:flex-row"}>
                 <div className={"w-full md:max-w-[200px] h-auto"}>
                     <img
-                        src={"./avatar/admin.webp"}
+                        src={"./avatar/admin.png"}
                         className={"w-full h-auto rounded-full"} // Full width on mobile
                         alt="Admin Avatar"
                     />
@@ -19,24 +19,24 @@ const ContentMaestroUi = () => {
                         <div className={"text-white text-l"}>Oleksandr Dosenko</div>
                     </div>
                     <div className={"text-white text-m flex flex-col gap-2 "}>
-                        <div className={"leading-[1]"}>
+                        <div className={"leading-[1] font-light"}>
                             Welcome to our student space at Kerry College in Tralee, Ireland. Whether you’re new or
                             a seasoned student, this site is your hub for student life. We’ve gathered everything you
                             need, from events and news to study resources and tips.
                         </div>
 
-                        <div className={"leading-[1]"}>
+                        <div className={"leading-[1] font-light"}>
                             Our mission is to enhance your college experience. This platform evolves to meet your
                             needs, so feel free to share updates or ideas. Your input keeps things fresh and relevant.
                         </div>
 
-                        <div className={"leading-[1]"}>
+                        <div className={"leading-[1] font-light"}>
                             Have a suggestion or see something we missed? Contact us! We’re here to listen and
                             improve, so every voice counts!
                         </div>
                         <div className={"leading-[1]"}>
-                            <a href="mailto:o.s.dosenko@gmail.com" className={"underline"}>
-                                o.s.dosenko@gmail.com
+                            <a href="mailto:admin@kerry.college" className={"underline"}>
+                                admin@kerry.college
                             </a>
                         </div>
                     </div>
