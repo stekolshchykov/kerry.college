60f7d5f8ceb594e73e646c2c8e2912154b1a4804
mk <v.a.stekolschikov@gmail.com>
2024-11-05 15:13:37 +0000

    added minio

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,9 +1,19 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'http://185.237.204.37:9000'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,96 @@
-export default () => ({});
+// module.exports = ({env}) => ({
+//     pagination: {
+//         pageSize: 1000,
+//         limit: 1000, // Установка лимита в 1000 записей
+//     },
+//     rest: {
+//         maxLimit: 1000,
+//         pageSize: 1000,
+//         limit: 1000,
+//     },
+//     // upload: {
+//     //     config: {
+//     //         provider: 'strapi-provider-upload-minio-ce',
+//     //         providerOptions: {
+//     //             accessKey: env('MINIO_ACCESS_KEY'),
+//     //             secretKey: env('MINIO_SECRET_KEY'),
+//     //             bucket: env('MINIO_BUCKET'),
+//     //             endPoint: env('MINIO_ENDPOINT'),
+//     //             port: env('MINIO_PORT'),
+//     //             useSSL: env('MINIO_USE_SSL'),
+//     //             host: env('MINIO_HOST'), // Удалите порт, если используется SSL
+//     //         },
+//     //     },
+//     // },
+// });
+
+module.exports = ({env}) => ({
+    pagination: {
+        pageSize: 1000,
+        limit: 1000, // Установка лимита в 1000 записей
+    },
+    rest: {
+        maxLimit: 1000,
+        pageSize: 1000,
+        limit: 1000,
+    },
+    upload: {
+        config: {
+            provider: 'strapi-provider-upload-minio-ce',
+            providerOptions: {
+                accessKey: "FwmBN1auqcVmrg77r5OM",
+                secretKey: "zJ9ykLhP7e0LKMOotvpj2yEKSQzrTNVh1KNzvN4I",
+                bucket: "kerry-college",
+                endPoint: "185.237.204.37",
+                useSSL: false,
+                port: 9000,
+                host: "185.237.204.37", // уберите :9002
+            },
+        },
+    },
+});
+
+
+// export default ({ env }: { env: (envKey: string) => string }) => ({
+//     upload: {
+//         config: {
+//             provider: 'strapi-provider-upload-ts-minio',
+//             providerOptions: {
+//                 accessKey: "FwmBN1auqcVmrg77r5OM",
+//                 secretKey: "zJ9ykLhP7e0LKMOotvpj2yEKSQzrTNVh1KNzvN4I",
+//                 bucket: "kerry-college",
+//                 endPoint: "185.237.204.37",
+//                 useSSL: false,
+//                 port: 9002,
+//                 host: "185.237.204.37", // уберите :9002
+//             },
+//         },
+//     },
+// });
+
+
+// module.exports = ({env}) => ({
+//     pagination: {
+//         pageSize: 1000,
+//         limit: 1000, // Установка лимита в 1000 записей
+//     },
+//     rest: {
+//         maxLimit: 1000,
+//         pageSize: 1000,
+//         limit: 1000,
+//     },
+//     upload: {
+//         config: {
+//             provider: 'strapi-provider-upload-minio-ce',
+//             providerOptions: {
+//                 accessKey: env('MINIO_ACCESS_KEY'),
+//                 secretKey: env('MINIO_SECRET_KEY'),
+//                 bucket: env('MINIO_BUCKET'),
+//                 endPoint: env('MINIO_ENDPOINT'),
+//                 port: env('MINIO_PORT'),
+//                 useSSL: env('MINIO_USE_SSL'),
+//                 host: env('MINIO_HOST'), // Удалите порт, если используется SSL
+//             },
+//         },
+//     },
+// });
