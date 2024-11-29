66e8061c4d56d8809326fdcd37cae5bc43f9dae1
mk <v.a.stekolschikov@gmail.com>
2024-11-29 10:47:07 +0000

    added TextareaUi

--- frontend/src/ui/contact-form.tsx
+++ frontend/src/ui/contact-form.tsx
@@ -1,4 +1,5 @@
 import InputUi from "@/ui/input-ui";
+import TextareaUi from "@/ui/textarea-ui";
 import React, {useState} from 'react';
 
 interface FormData {
@@ -73,20 +74,11 @@ const ContactForm: React.FC = () => {
                         value={formData.email}
                         onChange={email => setFormData({...formData, email: `${email}`})}/>
 
-                    <div>
-                        <label htmlFor="message" className="block text-sm font-medium text-gray-700">
-                            Message:
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            value={formData.message}
-                            onChange={handleChange}
-                            required
-                            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                            rows={4}
-                        />
-                    </div>
+                    <TextareaUi
+                        label={"Message"}
+                        value={formData.message}
+                        onChange={message => setFormData({...formData, message: `${message}`})}/>
+
                     <button
                         type="submit"
                         className="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md shadow"
--- frontend/src/ui/textarea-ui.tsx
+++ frontend/src/ui/textarea-ui.tsx
@@ -4,17 +4,17 @@ interface InputUiProps {
     label?: string;
     value?: string;
     onChange?: (e: string) => void;
+    rows?: number;
 }
 
-const InputUi = (props: InputUiProps) => {
+const TextareaUi = (props: InputUiProps) => {
     const id = useId();
     return (
         <div className={"w-full"}>
             {props?.label && <label htmlFor={id} className="block text-sm font-medium text-gray-700 w-full">
                 {props?.label}:
             </label>}
-            <input
-                type="text"
+            <textarea
                 id={id}
                 name="name"
                 value={props?.value}
@@ -23,11 +23,11 @@ const InputUi = (props: InputUiProps) => {
                         props?.onChange(event.target.value)
                     }
                 }}
-                required
+                rows={props?.rows || 4}
                 className="border border-gray-300 rounded-[5px] p-2 flex justify-between items-center bg-white w-full"
             />
         </div>
     );
 }
 
-export default InputUi;
+export default TextareaUi;
