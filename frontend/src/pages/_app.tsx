2d100e2ac2a4f11fb4b371756a53ccab1a5bfc6c
mk <v.a.stekolschikov@gmail.com>
2024-12-22 12:49:33 +0000

    added GA

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -14,7 +14,21 @@ function MyApp({Component, pageProps}: AppProps) {
     return (
         <RootStoreProvider>
             <Head>
+                
                 <title>Kerry College</title>
+                {/* Google Analytics */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3D30SMS9C"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-C3D30SMS9C');
+                        `,
+                    }}
+                />
+
             </Head>
             <HeaderUi/>
             <Component {...pageProps} />
